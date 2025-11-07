d785f206f572efdb4af52c5c5aeabedae485c593
Felipe de Jesus Perez Lopez <78581187+watatsumi23@users.noreply.github.com>
2025-11-07 02:04:03 -0600

    cambio 19

--- src/consts.ts
+++ src/consts.ts
@@ -32,14 +32,14 @@ export const SocialLinks = [
 export const WebsiteLinks = [
 	{
 		name: 'Home',
-		url: '/',
+		url: 'https://watatsumi23.github.io/',
 	},
 	{
 		name: 'Blog',
-		url: 'blog',
+		url: 'https://watatsumi23.github.io/blog/',
 	},
 	{
 		name: 'About',
-		url: 'about',
+		url: 'https://watatsumi23.github.io/about/',
 	},
 ]
